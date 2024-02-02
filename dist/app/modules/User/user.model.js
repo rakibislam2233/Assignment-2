b0725a24d99b6638fa626958226dac46f261c342
rakibislam2233 <rakib2020.tkg@gmail.com>
2024-02-02 21:08:37 +0600

    user model created

--- dist/app/modules/User/user.model.js
+++ dist/app/modules/User/user.model.js
@@ -1,2 +1,85 @@
 "use strict";
-//user model
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.User = void 0;
+const mongoose_1 = require("mongoose");
+//create fullname schema
+const FullNameSchema = new mongoose_1.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: [50, 'First name can not me more then 50 characters'],
+        validate: {
+            validator: function (value) {
+                const firstName = value.charAt(0).toUpperCase() + value.slice(1);
+                return firstName === value;
+            },
+            message: '{VALUE} is not capitalized formate',
+        },
+    },
+    lastName: { type: String, required: true, trim: true },
+});
+//create address schema
+const AddressSchema = new mongoose_1.Schema({
+    street: { type: String, required: true, trim: true },
+    city: { type: String, required: true, trim: true },
+    country: { type: String, required: true, trim: true },
+});
+//create order schema
+const OrderSchema = new mongoose_1.Schema({
+    productName: { type: String, required: true },
+    price: { type: Number, required: true, min: 0 },
+    quantity: { type: Number, required: true, min: 1 },
+});
+//create user schema
+const userSchema = new mongoose_1.Schema({
+    userId: {
+        type: Number,
+        required: [true, 'User id is required'],
+        unique: true,
+    },
+    username: {
+        type: String,
+        required: [true, 'Username is required'],
+        unique: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    fullName: {
+        type: FullNameSchema,
+        required: true,
+    },
+    age: {
+        type: Number,
+        required: true,
+        min: 0,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    isActive: {
+        type: Boolean,
+        required: true,
+        default: true,
+    },
+    hobbies: {
+        type: [String],
+        required: true,
+    },
+    address: {
+        type: AddressSchema,
+        required: true,
+    },
+    orders: {
+        type: [OrderSchema],
+        required: true,
+    },
+});
+//create model
+exports.User = (0, mongoose_1.model)('user', userSchema);
--- src/app/modules/User/user.model.ts
+++ src/app/modules/User/user.model.ts
@@ -1,1 +1,87 @@
-//user model
+import { Schema, model } from 'mongoose'
+import { TAddress, TFullName, TOrder, TUser } from './user.interface'
+
+//create fullname schema
+const FullNameSchema = new Schema<TFullName>({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: [50, 'First name can not me more then 50 characters'],
+    validate: {
+      validator: function (value: string) {
+        const firstName = value.charAt(0).toUpperCase() + value.slice(1)
+        return firstName === value
+      },
+      message: '{VALUE} is not capitalized formate',
+    },
+  },
+  lastName: { type: String, required: true, trim: true },
+})
+
+//create address schema
+const AddressSchema = new Schema<TAddress>({
+  street: { type: String, required: true, trim: true },
+  city: { type: String, required: true, trim: true },
+  country: { type: String, required: true, trim: true },
+})
+
+//create order schema
+const OrderSchema = new Schema<TOrder>({
+  productName: { type: String, required: true },
+  price: { type: Number, required: true, min: 0 },
+  quantity: { type: Number, required: true, min: 1 },
+})
+//create user schema
+const userSchema = new Schema<TUser>({
+  userId: {
+    type: Number,
+    required: [true, 'User id is required'],
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: [true, 'Username is required'],
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  fullName: {
+    type: FullNameSchema,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  isActive: {
+    type: Boolean,
+    required: true,
+    default: true,
+  },
+  hobbies: {
+    type: [String],
+    required: true,
+  },
+  address: {
+    type: AddressSchema,
+    required: true,
+  },
+  orders: {
+    type: [OrderSchema],
+    required: true,
+  },
+})
+
+//create model
+export const User = model<TUser>('user', userSchema)
